d2a1b7d7e220b940a4a78016cbebef6ec0637c79
eastGit <1832109788@qq.com>
2024-03-28 10:47:45 +0800

    hot-fix commit first

--- src/main/java/com/gitTest/gitTest.java
+++ src/main/java/com/gitTest/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("添加的代码");
+        System.out.println("hot-fix分支被修改");
     }
 }
